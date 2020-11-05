15926488b4416cd3e85ed1faf2ae5c252fef6fc6
joe62 <yunfei_z@163.com>
2020-11-05 11:11:50 +0800

    removed the sensitive items

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -6,20 +6,34 @@ using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
   public class Startup
   {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      // SECTION 1. Add code below
+      var builder = new NpgsqlConnectionStringBuilder();
+      builder.ConnectionString = Configuration
+        .GetConnectionString("PostgreSqlConnection");
+      builder.Username = Configuration["UserID"];
+      builder.Password = Configuration["Password"];
+      services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
       services.AddControllers();
-      services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+      services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
